5b4c542e20b0d743efe093eaa2c0654e27f99f59
Kakon2001 <jannatulkakon107@gmail.com>
2025-04-20 00:55:19 +0600

    backend files adding

---
+++ backend/api/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("Email is required")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("ac_role", 3)
+        return self.create_user(email, password, **extra_fields)
---
+++ backend/api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth import authenticate
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password", "ac_role"]
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            email = validated_data["email"],
+            password = validated_data["password"],
+            ac_role = validated_data.get("ac_role", 0),
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(email=data["email"], password=data["password"])
+        if user is not None:
+            raise serializers.ValidationError("Invalid email")
+        elif not user.is_active:
+            raise serializers.ValidationError("User is not active")
+        return user
